30c706d33265621083405bc1aa7c0971f76e5f23
871846173 <beimenlu30#>
2019-07-23 17:58:14 +0800

    Revert "Revert "Revert "修改获取收藏列表接口"""
    This reverts commit dd2b7c23dcb9755f6b20ae3f0bbbbaa1b7f0a3df.

--- src/main/java/cn/wildfirechat/app/CollectionServiceImpl.java
+++ src/main/java/cn/wildfirechat/app/CollectionServiceImpl.java
@@ -51,20 +51,16 @@ public class CollectionServiceImpl  implements CollectionService {
     }
 
     @Override
-    public RestResult getCollectionListWithUid(String uid) {
+    public List<Collection> getCollectionListWithUid(String uid) {
 //        int collectionCount=collectionDao.getCollectionCount(mid);
 //        List<Collection> collectionList=new ArrayList();
 //        for(int i=0;i<collectionCount;i++){
 //            Collection collection=new Collection();
 //            collection.setData();
 //        }
-
         List<Collection> list= collectionDao.getCollectionListWithUid(uid);
-        if(list==null){
-            return RestResult.error(RestResult.RestCode.ERROR_SERVER_ERROR);
-        }else {
-            return RestResult.ok(list);
-        }
+        return list;
+//        return  null;
     }
 
     @Override
